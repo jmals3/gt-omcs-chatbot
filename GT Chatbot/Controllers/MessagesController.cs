a041f71ebf1a0d85aea9ebb8b64ea43d1de30824
Jeff <Jeff@DESKTOP-2E4DEDF>
2016-11-21 20:23:01 -0500

    Add project files.

---
+++ GT Chatbot/Controllers/MessagesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace GT_Chatbot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                bool pastTopic = false;
+                Luis.GTLUIS gtLuis = await Luis.LUISGTOMCSClient.ParseUserInput(activity.Text);
+                string strRet = string.Empty;
+                string strTopic = activity.Text;
+
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                // Get the stateClient to get/set Bot Data
+                StateClient _stateClient = activity.GetStateClient();
+                BotData _botData = _stateClient.BotState.GetUserData(activity.ChannelId, activity.Conversation.Id);
+
+                switch (gtLuis.intents[0].intent)
+                {
+                    case "ThinkGradSchool":
+                        strRet = "Georgia Tech has a nice graduate school.";
+                        break;
+                    default:
+                        strRet = "Goodbye.";
+                        break;
+                }
+
+                Activity reply = activity.CreateReply(strRet);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+                return message.CreateReply("Hello bro");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ GT Chatbot/Luis.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Luis
+{
+    public class LUISGTOMCSClient
+    {
+        public static async Task<GTLUIS> ParseUserInput(string strInput)
+        {
+            string strRet = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://api.projectoxford.ai/luis/v2.0/apps/4eb7bb46-08f1-4e6f-89ef-7704564fbf6f?subscription-key=13b933f64b554ca0aac2d0225c53107c&q=" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<GTLUIS>(jsonResponse);
+                    return _Data;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class GTLUIS
+    {
+        public string query { get; set; }
+        public Topscoringintent topScoringIntent { get; set; }
+        public Intent[] intents { get; set; }
+        public object[] entities { get; set; }
+    }
+
+    public class Topscoringintent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+    }
+
+}
