3e5094819491bfbba547c838763054a85526cbd0
Jeff <Jeff@DESKTOP-2E4DEDF>
2016-11-30 22:37:56 -0500

    start to 3.2

---
+++ GT Chatbot/NLP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using edu.stanford.nlp;
+using edu.stanford.nlp.pipeline;
+
+namespace GT_Chatbot
+{
+    public class NLP
+    {
+        string text;
+
+        //public NLP(string input)
+        //{
+        //    text = input;
+        //}
+
+        //public static List<string> getTriple(string text)
+        //{
+        //    Annotation doc = new Annotation(text);
+
+        //}
+
+        public static void Main()
+        {
+            Debug.WriteLine("hey dude");
+        }
+
+
+    }
+}
